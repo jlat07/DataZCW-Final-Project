1a12b8e2cbdd57b0e6da2c3579f15498ffb71969
James Kocher <JKocher13@yahoo.com>
2020-05-11 21:53:11 -0400

    adding consumer

---
+++ kafka_twitter/consumer.py
@@ -0,0 +1,111 @@
+from kafka import KafkaConsumer, TopicPartition
+from json import loads
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import sessionmaker
+
+
+consumer = KafkaConsumer(
+    'tweet_stream',
+     bootstrap_servers=['localhost:9092'],
+     value_deserializer=lambda m: loads(m.decode('ascii')))
+
+
+engine = create_engine('mysql+pymysql://root:zipcoder@localhost/twitter')
+
+Base = declarative_base()
+class Tweets(Base):
+    __tablename__ = 'twitter_stream'
+    tweet_id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(250))
+    text = Column(String(250))
+    location = Column(String(250))
+    bounding = Column(String(300))
+    time_stamp  = Column(Integer)
+    state = Column(String(300))
+
+def get_state(loc_name):
+    locs = loc_name.split(",")
+    if "USA" in loc_name:
+        return locs[0]
+    else:
+        return states.get(locs[1].strip())
+
+states = {
+    'AK': 'Alaska',
+    'AL': 'Alabama',
+    'AR': 'Arkansas',
+    'AS': 'American Samoa',
+    'AZ': 'Arizona',
+    'CA': 'California',
+    'CO': 'Colorado',
+    'CT': 'Connecticut',
+    'DC': 'District of Columbia',
+    'DE': 'Delaware',
+    'FL': 'Florida',
+    'GA': 'Georgia',
+    'GU': 'Guam',
+    'HI': 'Hawaii',
+    'IA': 'Iowa',
+    'ID': 'Idaho',
+    'IL': 'Illinois',
+    'IN': 'Indiana',
+    'KS': 'Kansas',
+    'KY': 'Kentucky',
+    'LA': 'Louisiana',
+    'MA': 'Massachusetts',
+    'MD': 'Maryland',
+    'ME': 'Maine',
+    'MI': 'Michigan',
+    'MN': 'Minnesota',
+    'MO': 'Missouri',
+    'MP': 'Northern Mariana Islands',
+    'MS': 'Mississippi',
+    'MT': 'Montana',
+    'NA': 'National',
+    'NC': 'North Carolina',
+    'ND': 'North Dakota',
+    'NE': 'Nebraska',
+    'NH': 'New Hampshire',
+    'NJ': 'New Jersey',
+    'NM': 'New Mexico',
+    'NV': 'Nevada',
+    'NY': 'New York',
+    'OH': 'Ohio',
+    'OK': 'Oklahoma',
+    'OR': 'Oregon',
+    'PA': 'Pennsylvania',
+    'PR': 'Puerto Rico',
+    'RI': 'Rhode Island',
+    'SC': 'South Carolina',
+    'SD': 'South Dakota',
+    'TN': 'Tennessee',
+    'TX': 'Texas',
+    'UT': 'Utah',
+    'VA': 'Virginia',
+    'VI': 'Virgin Islands',
+    'VT': 'Vermont',
+    'WA': 'Washington',
+    'WI': 'Wisconsin',
+    'WV': 'West Virginia',
+    'WY': 'Wyoming'
+}
+
+for message in consumer:
+    print("received")
+    message = message.value
+    tweet_id = message.get("id")
+    name = message.get("user").get("name")
+    text = message.get("text")
+    place = message.get("place")
+    location = place.get("full_name")
+    bounding = place.get("bounding_box").get("coordinates")
+    time_stamp = message.get("timestamp_ms")
+    state = get_state(location)
+    message_sql = Tweets(tweet_id=tweet_id, name=name, text=text, location = location, bounding= bounding, time_stamp = time_stamp, state = state)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    session.add(message_sql)
+    session.commit()
+
