58ddc0ee7cdfa5056a385413dd37708b9faee618
James Kocher <JKocher13@yahoo.com>
2020-05-11 21:08:27 -0400

    edit

--- kafka_twitter/producer.py
+++ kafka_twitter/producer.py
@@ -7,8 +7,8 @@ consumer_secret = "insert"
 acc_token = "insert"
 acc_sec_token = "insert"
 
-api = twitter.Api(consumer_key='fIaCWYCoyKd3tlm0jNVoFMxn6', consumer_secret='TgGTIm2W6V7DmQQQ55qcEZk47cxzaaGzkcZDVpyiGPC927LW6u' , access_token_key='755394958345969664-gdZQQQrTbb2CybZt5rX5unQb4AMZgak' , access_token_secret='Fsw2F87btwuXpmLdsm9kUnxvsHXAHLmPJYOkasXtb7uU6')
-#api = twitter.Api(consumer_key= cons_key , consumer_secret=consumer_secret , access_token_key= acc_token , access_token_secret= acc_sec_token)
+
+api = twitter.Api(consumer_key= cons_key , consumer_secret=consumer_secret , access_token_key= acc_token , access_token_secret= acc_sec_token)
 results = api.GetStreamFilter(locations=["-178.334698, 18.910361, -66.949895, 71.41286","-167.21211, 53.24541, -140.93442, 71.365162"], languages=['en'], filter_level="low")
 
 producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
