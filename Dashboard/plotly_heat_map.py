f0cad480238716b397488504e6e6fe431d0d341a
James LA Thompson <jlat0128@gmail.com>
2020-05-18 15:17:45 -0400

    updating heat map

--- Dashboard/plotly_heat_map.py
+++ Dashboard/plotly_heat_map.py
@@ -12,57 +12,23 @@ app = dash.Dash(__name__)
 
 # ------------------------------------------------------------------------------
 # Import and clean data (importing csv into pandas)
-df = pd.read_csv("/Users/jthompson/dev/DataZCW-Final-Project/Data/twitter_data_analysis2020-05-09-18.csv")
+df = pd.read_csv("/Users/jthompson/dev/DataZCW-Final-Project/Dashboard/date_tweets_locations_50.csv")
 
-# print(df)
+print(df)
 
 # ------------------------------------------------------------------------------
 # App layout
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-app.layout = html.Div([
-    dcc.Slider(
-        id='my-slider',
-        min=0,
-        max=20,
-        step=0.5,
-        value=10,
-    ),
-    html.Div(id='slider-output-container')
-])
-
-
-@app.callback(
-    dash.dependencies.Output('slider-output-container', 'children'),
-    [dash.dependencies.Input('my-slider', 'value')])
-def update_output(value):
-    return 'You have selected "{}"'.format(value)
-
-
-
-
-
 app.layout = html.Div([
 
     html.H1("Covid-19 Sentiment Dasboard", style={'text-align': 'center'}),
 
-    dcc.Dropdown(id="slct_month",
-                 options=[
-                     {"label": "Jan", "value": 1},
-                     {"label": "Feb", "value": 2},
-                     {"label": "Mar", "value": 3},
-                     {"label": "Apr", "value": 4},
-                     {"label": "May", "value": 5},
-                     {"label": "Jun", "value": 6},
-                     {"label": "Jul", "value": 7},
-                     {"label": "Aug", "value": 8},
-                     {"label": "Sep", "value": 9},
-                     {"label": "Oct", "value": 10},
-                     {"label": "Nov", "value": 11},
-                     {"label": "Dec", "value": 12}],
+    dcc.Dropdown(id="select_date",
+                 options=[{"label": "2020-05-09", "value": 2020-5-9},
+                        {"label": "2020-05-10", "value": 2020-5-10},
+                        {"label": "2020-05-11", "value": 2020-5-11},
+                        ],
                  multi=False,
-                 value=1,
+                 value=2020-5-9,
                  style={'width': "40%"}
                  ),
 
@@ -70,50 +36,49 @@ app.layout = html.Div([
     html.Br(),
 
     dcc.Graph(id='Sentiment_Map', figure={})
-
 ])
-
 
 # ------------------------------------------------------------------------------
 # Connect the Plotly graphs with Dash Components
 @app.callback(
-    [Output(component_id='slider-output-container', component_property='children'),
+    [Output(component_id='output_container', component_property='children'),
      Output(component_id='Sentiment_Map', component_property='figure')],
-    [Input(component_id='slct_month', component_property='value')]
+    [Input(component_id='select_date', component_property='value')]
 )
-def update_graph(option_slctd):
-    print(option_slctd)
-    print(type(option_slctd))
+def update_graph(option_select):
+    print(option_select)
+    print(type(option_select))
 
-    container = "The month chosen by user was: {}".format(option_slctd)
+
+    container = f"The day chosen by user was: {option_select}"
 
     dff = df.copy()
-    dff = dff[dff["Month"] == option_slctd]
-    dff = dff[dff["Sentiment"] == "Positive"]
+    dff = dff[dff["date"] == option_select]
+    # dff = dff[dff["sentiment"] == "Positive"]
 
     # Plotly Express
     fig = px.choropleth(
         data_frame=dff,
         locationmode='USA-states',
-        locations='state_code',
+        locations='location_abbreviation',
         scope="usa",
-        color='Sentiment',
-        hover_data=['State', 'Sentiment'],
+        color='sentiment',
+        hover_data=['location', 'sentiment'],
         color_continuous_scale=px.colors.sequential.YlOrRd,
         labels={'Sentiment Value': 'Sentiment'},
         template='plotly_dark'
     )
 
-    # Plotly Graph Objects (GO)
+    #Plotly Graph Objects (GO)
     # fig = go.Figure(
     #     data=[go.Choropleth(
     #         locationmode='USA-states',
-    #         locations=dff['state_code'],
-    #         z=dff["Sentiment"].astype(float),
+    #         locations=dff['location_abbreviation'],
+    #         z=dff['sentiment'].astype(float),
     #         colorscale='Reds',
     #     )]
     # )
-    #
+    
     # fig.update_layout(
     #     title_text="Title",
     #     title_xanchor="center",
